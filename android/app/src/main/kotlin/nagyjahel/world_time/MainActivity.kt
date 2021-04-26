3492d2a397d3d9c10d7ad6cbe659bcfdb629c02c
Nagy Jáhel <nagy.jahel@accenture.com>
2021-04-26 14:20:53 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/nagyjahel/world_time/MainActivity.kt
@@ -0,0 +1,6 @@
+package nagyjahel.world_time
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/loading.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+  String time = 'loading';
+  void setUpWorldTime() async{
+    WorldTime instance = WorldTime(location: 'Bucharest', flag: 'romania.png', url: 'Europe/Bucharest');
+    await instance.getTime();
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDayTime' : instance.isDayTime
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setUpWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[900],
+      body: Center(
+        child: SpinKitFadingCube(
+          color: Colors.white,
+          size: 80.0,
+        )
+      )
+    );
+  }
+}
